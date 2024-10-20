60949df20cad010b1fcb7c56b5486cd5a564777d
arkadykorsakov <arkadiy.korsakov.98@mail.ru>
2024-10-20 13:45:24 +0700

    first commit

---
+++ src/stores/score.js
@@ -0,0 +1,47 @@
+import { defineStore } from 'pinia'
+
+const baseLevelScore = 25
+
+const levels = new Array(15)
+  .fill(0)
+  .map((_, i) => baseLevelScore * Math.pow(2, i))
+
+const levelScores = levels.reduce((acc, value) => {
+  const lastSum = acc.at(-1) || 0
+  acc.push(lastSum + value)
+  return acc
+}, [])
+
+function computeLevelByScore(score) {
+  for (let [index, value] of levelScores.entries()) {
+    if (score <= value) {
+      return {
+        level: index,
+        value: levels[index]
+      }
+    }
+  }
+}
+
+export const useScoreStore = defineStore('score', {
+  state: () => ({
+    score: 0
+  }),
+  getters: {
+    level: (state) => computeLevelByScore(state.score),
+    currentScore(state) {
+      if (this.level.level === 0) {
+        return state.score
+      }
+      return state.score - levelScores[this.level.level - 1]
+    }
+  },
+  actions: {
+    add(score = 1) {
+      this.score += score
+    },
+    setScore(score) {
+      this.score = score
+    }
+  }
+})
